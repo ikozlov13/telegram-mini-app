ae6cd201dfa727b4c02fcbf2ff01fc98764feb00
ikozlov13 <icozlov13@icloud.com>
2025-02-21 22:12:47 +0300

    Добавлен телеграм мини апп

--- server.js
+++ server.js
@@ -15,3 +15,19 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Сервер запущен на порту ${PORT}`);
 });
+
+const TelegramBot = require('node-telegram-bot-api');
+const token = '8040346486:AAFHLxeHFlzmFFs0nNPr2JVbmgsk9XgotJo';
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+
+    bot.sendMessage(chatId, "Нажмите кнопку ниже, чтобы открыть Mini App", {
+        reply_markup: {
+            inline_keyboard: [
+                [{ text: "Открыть Mini App", web_app: { url: "https://telegram-mini-app.onrender.com" } }]
+            ]
+        }
+    });
+});
